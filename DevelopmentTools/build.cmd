b3f92720ab3e39da4d7cef0a539b69a42ace9b0f
JamesMc <jamesjohnmcguire@gmail.com>
2021-12-09 12:22:07 +0900

    Add release section to build scripts
    If specifying the release parameter, the scripts will attempt to build
    and release an actual published version of the package.

--- DevelopmentTools/build.cmd
+++ DevelopmentTools/build.cmd
@@ -1,9 +1,43 @@
 CD %~dp0
-CD ..\SourceCode
+CD ..
+
+CALL composer install
+
+ECHO composer outdated packages:
+CALL composer outdated
+
+CD SourceCode
 
 ECHO PHP code styles
 CALL ..\vendor\bin\phpcs -sp --standard=ruleset.xml .
 
 CD ..
+
+if "%1" == "release" GOTO release
+GOTO end
+
+:release
+ECHO Release
+
 REM Currently, not working on windows - phpdocumentor bug
 REM CALL phpdocumentor --setting=graphs.enabled=true -d SourceCode -t Documentation
+
+if "%~2"=="" GOTO error1
+if "%~3"=="" GOTO error2
+
+git tag %2
+git push --tags
+git push --all
+
+gh release create %2 --notes "%3"
+
+GOTO end
+
+:error1
+ECHO No tag specified
+GOTO end
+
+:error2
+ECHO No message specified
+
+:end
--- DevelopmentTools/build.sh
+++ DevelopmentTools/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-cd ../SourceCode
+cd ..
+
+composer install
+
+echo composer outdated packages:
+composer outdated
+
+cd SourceCode
 
 echo PHP code styles
 ../vendor/bin/phpcs -sp --standard=ruleset.xml .
@@ -11,4 +18,22 @@ if [[ $1 == "release" ]] ; then
 
 	rm -rf Documentation
 	phpDocumentor.phar --setting="graphs.enabled=true" -d SourceCode -t Documentation
+
+	if [ -z "$2" ]
+	then
+		echo "No tag specified"
+		exit 1
+	fi
+
+	if [ -z "$3" ]
+	then
+		echo "No message specified"
+		exit 1
+	fi
+
+	git tag $2
+	git push --tags
+	git push --all
+
+	gh release create $2 --notes $3
 fi
