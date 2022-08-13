78514c9e2a5870c662247942621c6ed31108c969
James John McGuire <jamesjohnmcguire@gmail.com>
2022-08-13 18:10:51 +0900

    Simplefly example

--- Example/OldDatabase.php
+++ Example/OldDatabase.php
@@ -1,40 +1,24 @@
 <?php
-require_once "debug.php";
 require_once "vendor/digitalzenworks/consumer-provider/SourceCode/ProviderInterface.php";
 require_once "DatabaseLibrary.php";
 
 class OldDatabase implements digitalzenworks\ConsumerProvider\ProviderInterface
 {
-	private $consumer = null;
-	private $debug = null;
-	private $inException = false;
-	private $list = array();
 	private $oldDatabase = null;
 
 	public function __construct()
 	{
-		$this->name = 'Old Database';
-
-		$logFile = __DIR__ . '/Import.log';
-		$this->debug = new Debug(Debug::DEBUG, $logFile);
-
 		$this->oldDatabase = new DatabaseLibrary(
 			'localhost', 'example_old_database', 'example_user',
-			'example_password', false, $this->debug);
+			'example_password');
 	}
 
-	public function Process($consumer)
+	public function Process()
 	{
-		$this->debug->Show(Debug::DEBUG, "Provider Processing...");
-
-		$this->consumer = $consumer;
-
-		$query = 'select * from tbl_news';
+		$query = 'select * from News';
 
 		$count = $this->oldDatabase->GetAll($query, $articles);
 
-		$this->list = $consumer->Process($articles);
-
-		return $this->list;
+		return $articles;
 	}
 }
